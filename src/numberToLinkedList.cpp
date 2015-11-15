b1815d73c81129fe26d21a408c5fab9255a4e022
Rohith24 <rohithreddy24@hotmail.com>
2015-11-15 15:21:15 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -19,6 +19,33 @@ struct node {
 	struct node *next;
 };
 
+typedef struct node *list;
+typedef struct node *position;
+
+void insert(list ll, position p, int e);
+
 struct node * numberToLinkedList(int N) {
-	return NULL;
+	list head = (struct node *)malloc(sizeof(struct node));
+	head->next = NULL;
+	if (N == 0){
+		head->num = 0;
+		return head;
+	}
+	if (N<0)
+		N *= -1;
+	while (N>0)
+	{
+		insert(head, head, N%10);
+		N /= 10;
+	}
+	return head->next;
+}
+
+void insert(list ll, position p, int e)
+{
+	list temp;
+	temp = (list)malloc(sizeof(struct node));
+	temp->num = e;
+	temp->next = p->next;
+	p->next = temp;
 }
